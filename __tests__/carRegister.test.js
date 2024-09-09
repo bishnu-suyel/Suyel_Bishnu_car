373531c38805268a2780dc85099fcc2f663cade8
Suyel Bishnu <suyel2007@gmail.com>
2024-09-09 14:54:47 +0300

    Create a constructor test

--- __tests__/carRegister.test.js
+++ __tests__/carRegister.test.js
@@ -1,1 +1,10 @@
-// 
+"use strict";
+
+const carRegister = require("../carRegister");
+const defaultData = require("../cars.json");
+
+describe("Testing constructor", () => {
+  test("Test 1: missing parameter throw an exception", () => {
+    expect(() => new carRegister()).toThrow('data storage missing');
+  });
+});
