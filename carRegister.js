a0180c7f0172ebeb5b9491a46310303d0ba41a4b
Suyel Bishnu <suyel2007@gmail.com>
2024-09-09 14:51:25 +0300

    Initialize the project

---
+++ carRegister.js
@@ -0,0 +1,11 @@
+// "use strict";
+
+// module.exports = class carRegister {
+//   #register;
+//   constructor(data) {
+//     if (!data) {
+//       throw new Error("data storage missing");
+//     }
+//     this.#register = data;
+//   }
+// };
