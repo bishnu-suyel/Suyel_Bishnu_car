0f8cf70b457d7e042933c38ff1463c67950f4c8e
Suyel Bishnu <suyel2007@gmail.com>
2024-09-10 00:08:18 +0300

    Create test for getTotalPrice OfCarsByModel

--- __tests__/carRegister.test.js
+++ __tests__/carRegister.test.js
@@ -7,4 +7,28 @@ describe("Testing constructor", () => {
   test("Test 1: missing parameter throw an exception", () => {
     expect(() => new carRegister()).toThrow('data storage missing');
   });
-});
+}); // End of testing constructor.
+
+describe("Test cases for getTotalPriceOfCarsByModel", () => {
+  test("Test 1: VIP", () => {
+    const register = new carRegister(defaultData);
+    expect(register.getTotalPriceOfCarsByModel("VIP")).toBe(300);
+  });
+
+  test("Test 2: silver", () => {
+    const register = new carRegister(defaultData);
+    expect(register.getTotalPriceOfCarsByModel("silver")).toBe(159); //36+123
+  });
+
+  test("Test 3: nothing found with given searchValue", () => {
+    const register = new carRegister(defaultData);
+    expect(() => register.getTotalPriceOfCarsByModel("x")).toThrow(
+      "nothing found with given searchValue"
+    );
+  });
+
+  test("Test 4: parameter missing", ()=>{
+    const register = new carRegister(defaultData);
+    expect(() => register.getTotalPriceOfCarsByModel("")).toThrow('missing parameter');
+  });
+}); // End of testing getTotalPriceOfCarsByModel
