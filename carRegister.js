94027dd83b60295aa0838888c09e1987538bee53
Suyel Bishnu <suyel2007@gmail.com>
2024-09-10 00:07:45 +0300

    Create implementation for getTotalPriceOfCarsByModel

--- carRegister.js
+++ carRegister.js
@@ -8,4 +8,27 @@ module.exports = class carRegister {
     }
     this.#register = data;
   }
+
+  // Start of getTotalPriceOfCarsByModel
+  getTotalPriceOfCarsByModel(searchValue){
+    if (!searchValue) {
+      throw new Error("missing parameter");
+    }
+    // Filter cars by the given model
+    const filteredCars = this.#register.filter(
+      (car) => car.model === searchValue
+    );
+
+    // If no cars match the searchValue, throw an error
+    if (filteredCars.length === 0) {
+      throw new Error("nothing found with given searchValue");
+    }
+
+    // Calculate the total price of the filtered cars
+    const totalPrice = filteredCars.reduce((sum, car) => sum + car.price, 0);
+    return totalPrice;
+  }
+  // End of getTotalPriceOfCarsByModel
+
+  // Start of getInfo
 };
