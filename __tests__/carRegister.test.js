e89c5b870bcc6242c1ad758f6e0d9e93a40118c2
Suyel Bishnu <suyel2007@gmail.com>
2024-09-10 22:50:55 +0300

    Create test for getInfo function

--- __tests__/carRegister.test.js
+++ __tests__/carRegister.test.js
@@ -7,8 +7,10 @@ describe("Testing constructor", () => {
   test("Test 1: missing parameter throw an exception", () => {
     expect(() => new carRegister()).toThrow('data storage missing');
   });
-}); // End of testing constructor.
+}); 
+// End of testing constructor.
 
+//Start of testing getTotalPriceOfCarsByModel
 describe("Test cases for getTotalPriceOfCarsByModel", () => {
   test("Test 1: VIP", () => {
     const register = new carRegister(defaultData);
@@ -31,4 +33,47 @@ describe("Test cases for getTotalPriceOfCarsByModel", () => {
     const register = new carRegister(defaultData);
     expect(() => register.getTotalPriceOfCarsByModel("")).toThrow('missing parameter');
   });
-}); // End of testing getTotalPriceOfCarsByModel
+}); 
+// End of testing getTotalPriceOfCarsByModel
+
+//Start of getInfo
+describe("Test cases for getInfo", () => {
+  test("Test 1: searchKey 1", () => {
+    const register = new carRegister(defaultData);
+    const expectedResult = {
+      consumptionPer100km: 1,
+      comments: "low emission",
+      powerSource: "pushing",
+    };
+    expect(register.getInfo("1")).toEqual(expectedResult);
+  });
+
+  test("Test 2: searchKey 2", () => {
+    const register = new carRegister(defaultData);
+    const expectedResult = {
+      consumptionPer100km: 4,
+      comments: "-",
+      powerSource: "hybrid",
+    };
+    expect(register.getInfo("2")).toEqual(expectedResult);
+  });
+
+  test("Test 3: wrong searchKey", () => {
+    const register = new carRegister(defaultData);
+    expect(() => register.getInfo("10")).toThrow(
+      "nothing found with given searchKey");
+  });
+
+  test("Test 4: empty searchKey", () => {
+    const register = new carRegister(defaultData);
+    expect(() => register.getInfo("")).toThrow("missing parameter");
+  });
+
+  test("Test 5: searchKey 5 (no info field)", () => {
+    const register = new carRegister(defaultData);
+    expect(() => register.getInfo("5")).toThrow(
+      "No information available for this car");
+  });
+});
+
+// End of getInfo
